895b0d0621a3a1159714ee8454b667f5a273bb2c
Measum-Shah <shahmeasum008@gmail.com>
2025-03-22 19:15:41 +0500

    Project Setup Completed

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${monaSans.className}ntialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
