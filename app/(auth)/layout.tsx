c2673f22e1a756ae8a00bb23cb9badd84e9ea3d7
Measum-Shah <shahmeasum008@gmail.com>
2025-03-22 21:14:53 +0500

    we completed basic routing structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ReactNode} from 'react';
+
+const AuthLayout = ({children}:{children: ReactNode}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AuthLayout
